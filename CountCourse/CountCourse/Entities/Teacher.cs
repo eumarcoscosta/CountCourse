130b047e6e9d1ecaf5f3598b5863b96901602566
Marcos <marcoscosta@air-de-marcos.lan>
2024-08-18 13:18:33 +0100

    Complete app

---
+++ CountCourse/CountCourse/Entities/Teacher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace countCourse.Entities
+{
+	public class Teacher 
+	{
+
+		public int Course { get; set; }
+        public int IdCourse { get; set; }
+
+        public Teacher(int course, int idCourse) 
+        {
+            Course = course;
+            IdCourse = idCourse;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Teacher))
+            {
+                return false;
+            }
+            Teacher other = obj as Teacher;
+            return IdCourse.Equals(other.IdCourse);
+        }
+
+        public override int GetHashCode()
+        {
+            return IdCourse.GetHashCode();
+        }
+
+        public Teacher()
+		{
+		}
+
+        public Teacher(int idCourse)
+        {
+            IdCourse = idCourse;
+        }
+    }
+}
+
---
+++ CountCourse/CountCourse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using countCourse.Entities;
+using System.Collections.Generic;
+
+namespace countCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<Teacher> quant = new HashSet<Teacher>();
+
+            Console.Write("How many students for course A? ");
+            int nA = int.Parse(Console.ReadLine());
+
+            for (int i=0; i<nA; i++)
+            {
+                int idCourse = int.Parse(Console.ReadLine());
+                quant.Add(new Teacher(idCourse));
+            }
+
+            Console.Write("How many students for course B? ");
+            int nB = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nB; i++)
+            {
+                int idCourse = int.Parse(Console.ReadLine());
+                quant.Add(new Teacher(idCourse));
+            }
+
+            Console.Write("How many students for course C? ");
+            int nC = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nC; i++)
+            {
+                int idCourse = int.Parse(Console.ReadLine());
+                quant.Add(new Teacher(idCourse));
+            }
+
+            Console.WriteLine("Total students: " + quant.Count);
+        }
+    }
+}
